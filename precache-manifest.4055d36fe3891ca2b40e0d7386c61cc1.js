eac96cfa499f4d3dd498958175c263263032d51a
Labiba Ibnat <labiba.ssp@gmail.com>
2020-10-07 17:19:29 +0600

    Updates

---
+++ precache-manifest.4055d36fe3891ca2b40e0d7386c61cc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70d3dd7caad80a16e7168c1b0cbfb454",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "f4366558a501420ed4f2",
+    "url": "/monster-rolodex/static/css/main.581e6d60.chunk.css"
+  },
+  {
+    "revision": "d0c3834aa20bea82f71f",
+    "url": "/monster-rolodex/static/js/2.bfbad1a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.bfbad1a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4366558a501420ed4f2",
+    "url": "/monster-rolodex/static/js/main.94c88319.chunk.js"
+  },
+  {
+    "revision": "d605007852bfce40337c",
+    "url": "/monster-rolodex/static/js/runtime-main.62c67171.js"
+  }
+]);
